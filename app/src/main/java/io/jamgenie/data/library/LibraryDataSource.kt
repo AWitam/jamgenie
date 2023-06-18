88a0f4a84cbee382294e79ff10cf350a83eb7f35
Ada Witamborska <adrianna@tonik.com>
2023-06-18 23:01:10 +0200

    refactor: add more static data to display

--- app/src/main/java/io/jamgenie/data/library/LibraryDataSource.kt
+++ app/src/main/java/io/jamgenie/data/library/LibraryDataSource.kt
@@ -4,8 +4,7 @@ import io.jamgenie.data.home.User
 
 class LibraryDataSource {
     val user1 = User("john_doe", "admin")
-    val user2 = User("stacy", "admin")
-    val user3 = User("jane_smith", "artist")
+    val user2 = User("kate_m", "admin")
 
     val imageUrl1 = "https://images.pexels.com/photos/3987251/pexels-photo-3987251.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
     val imageUrl2 = "https://images.pexels.com/photos/2157171/pexels-photo-2157171.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
@@ -18,20 +17,20 @@ class LibraryDataSource {
         durationInSeconds = 30,
         isPublic = true,
         level = null,
-        imageUrl = imageUrl1,
+        imageUrl = "https://images.pexels.com/photos/4426320/pexels-photo-4426320.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load",
         video = null
     )
 
 
     private val practiceItem2 = PracticeItem(
         id = "PI_2",
-        creator = user1,
+        creator = user2,
         title = "Major chord changes",
         description = "Pick two major chords and practice changing between them. Example: C major and G major. or D major and A major.",
         durationInSeconds = 60,
         isPublic = true,
         level = Level.BRAND_NEW,
-        imageUrl = imageUrl1,
+        imageUrl = "https://images.pexels.com/photos/374698/pexels-photo-374698.jpeg?auto=compress&cs=tinysrgb&w=800&h=600&dpr=1",
         video = null
     )
 
@@ -44,7 +43,7 @@ class LibraryDataSource {
         durationInSeconds = 120,
         isPublic = true,
         level = Level.BRAND_NEW,
-        imageUrl = imageUrl1,
+        imageUrl = "https://images.pexels.com/photos/1933589/pexels-photo-1933589.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
         video = null
     )
 
@@ -56,7 +55,7 @@ class LibraryDataSource {
         durationInSeconds = 600,
         isPublic = true,
         level = null,
-        imageUrl = imageUrl1,
+        imageUrl = "https://images.pexels.com/photos/8182363/pexels-photo-8182363.jpeg",
         video = null
     )
 
@@ -74,7 +73,7 @@ class LibraryDataSource {
 
     private val practiceItem6 = PracticeItem(
         id = "PI_6",
-        creator = user1,
+        creator = user2,
         title = "Sus chord changes",
         description = "Work on your sus chords changes. Example: C major and C sus4. or D sus2 and D sus4.",
         durationInSeconds = 120,
@@ -93,6 +92,90 @@ class LibraryDataSource {
         isPublic = true,
         level = Level.BEGINNER,
         imageUrl = imageUrl1,
+        video = null
+    )
+
+    val practiceItem8 = PracticeItem(
+        id = "PI_8",
+        creator = user1,
+        title = "Strumming Patterns",
+        description = "Practice different strumming patterns to improve your rhythm skills.",
+        durationInSeconds = 300,
+        isPublic = true,
+        level = Level.BEGINNER,
+        imageUrl = imageUrl2,
+        video = null
+    )
+
+    val practiceItem9 = PracticeItem(
+        id = "PI_9",
+        creator = user1,
+        title = "Chord Progressions",
+        description = "Practice common chord progressions to enhance your chord transitioning skills.",
+        durationInSeconds = 240,
+        isPublic = true,
+        level = Level.INTERMEDIATE,
+        imageUrl = imageUrl2,
+        video = null,
+    )
+
+    val practiceItem10 = PracticeItem(
+        id = "PI_3",
+        creator = user2,
+        title = "Picking Exercises",
+        description = "Improve your picking technique with a variety of picking exercises.",
+        durationInSeconds = 45,
+        isPublic = true,
+        level = Level.BEGINNER,
+        imageUrl = "https://images.pexels.com/photos/4088014/pexels-photo-4088014.jpeg?auto=compress&cs=tinysrgb&w=800",
+        video = null
+    )
+
+    val practiceItem11= PracticeItem(
+        id = "PI_11",
+        creator = user2,
+        title = "Barre Chord Practice",
+        description = "Develop strength and accuracy in playing barre chords with targeted exercises.",
+        durationInSeconds = 90,
+        isPublic = true,
+        level = Level.INTERMEDIATE,
+        imageUrl = "https://images.pexels.com/photos/4472065/pexels-photo-4472065.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+        video = null
+    )
+
+    val practiceItem12 = PracticeItem(
+        id = "PI_12",
+        creator = user1,
+        title = "Scales Mastery",
+        description = "Master major and minor scales across the fretboard with scale exercises.",
+        durationInSeconds = 1200,
+        isPublic = true,
+        level = Level.ADVANCED,
+        imageUrl = "https://images.pexels.com/photos/4426320/pexels-photo-4426320.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load",
+        video = null
+    )
+
+    val practiceItem13 = PracticeItem(
+        id = "PI_6",
+        creator = user2,
+        title = "Fingerpicking Patterns",
+        description = "Learn and practice popular fingerpicking patterns to enhance your fingerstyle playing.",
+        durationInSeconds = 240,
+        isPublic = true,
+        level = Level.INTERMEDIATE,
+        imageUrl = "https://images.pexels.com/photos/7462024/pexels-photo-7462024.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load",
+        video = null
+    )
+
+    val practiceItem14 = PracticeItem(
+        id = "PI_14",
+        creator = user1,
+        title = "Strumming Techniques",
+        description = "Explore various strumming techniques to add dynamics and rhythm to your playing.",
+        durationInSeconds = 45,
+        isPublic = true,
+        level = Level.INTERMEDIATE,
+        imageUrl = "https://images.pexels.com/photos/7462024/pexels-photo-7462024.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load",
         video = null
     )
 
@@ -127,7 +210,42 @@ class LibraryDataSource {
             practiceItem4
         ),
         level = Level.BEGINNER,
-        imageUrl = imageUrl2,
+        imageUrl = "https://images.pexels.com/photos/12616312/pexels-photo-12616312.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load",
+        popularity = 0,
+        isPublic = true
+    )
+
+    val chordsRoutine = Routine(
+        id = "R_3",
+        creator = user2,
+        title = "Fundamental Chords Practice",
+        description = "This routine focuses on practicing fundamental chords for beginner guitarists.",
+        practiceItems = listOf(
+            practiceItem3,
+            practiceItem7,
+            practiceItem8,
+            practiceItem10
+        ),
+        level = Level.BEGINNER,
+        imageUrl = "https://images.pexels.com/photos/374698/pexels-photo-374698.jpeg?auto=compress&cs=tinysrgb&w=800&h=600&dpr=1",
+        popularity = 0,
+        isPublic = true
+    )
+
+    val fingerstyleRoutine = Routine(
+        id = "R_4",
+        creator = user2,
+        title = "Fingerstyle Techniques",
+        description = "This routine introduces basic fingerstyle techniques for beginner guitarists.",
+        practiceItems = listOf(
+            practiceItem1,
+            practiceItem5,
+            practiceItem8,
+            practiceItem11,
+            practiceItem10
+        ),
+        level = Level.INTERMEDIATE,
+        imageUrl = "https://images.pexels.com/photos/4472065/pexels-photo-4472065.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
         popularity = 0,
         isPublic = true
     )
@@ -144,6 +262,8 @@ class LibraryDataSource {
 
     val allRoutines = listOf(
         brandNewLevelRoutine,
+        chordsRoutine,
+        fingerstyleRoutine,
         beginnerLevelRoutine
     )
 
--- app/src/main/java/io/jamgenie/ui/home/HomeViewModel.kt
+++ app/src/main/java/io/jamgenie/ui/home/HomeViewModel.kt
@@ -1,19 +1,52 @@
 package io.jamgenie.ui.home
 
+import android.os.Bundle
+import androidx.lifecycle.AbstractSavedStateViewModelFactory
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.savedstate.SavedStateRegistryOwner
 import io.jamgenie.data.home.UserRepository
+import io.jamgenie.data.library.LibraryRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 
-class HomeViewModel : ViewModel() {
+class HomeViewModel(
+    private val userRepository: UserRepository,
+) : ViewModel() {
     private val _uiState =
         MutableStateFlow(HomeUIState())
     val uiState: StateFlow<HomeUIState> = _uiState.asStateFlow()
 
     init {
-        val user = UserRepository().getUser()
-        _uiState.value = HomeUIState(user)
+        val user = userRepository.getUser()
+        val currentPracticeRoutineTitle = user.currentPracticeRoutineId?.let {
+            LibraryRepository().getLibraryItem(
+                it
+            ).title
+        } ?: "No practice routine selected"
+        _uiState.value = HomeUIState(user, currentPracticeRoutineTitle)
+    }
+
+    companion object {
+        fun provideFactory(
+
+            owner: SavedStateRegistryOwner,
+            defaultArgs: Bundle? = null,
+
+            ): AbstractSavedStateViewModelFactory =
+            object : AbstractSavedStateViewModelFactory(owner, defaultArgs) {
+                override fun <T : ViewModel> create(
+                    key: String,
+                    modelClass: Class<T>,
+                    handle: SavedStateHandle
+                ): T {
+                    @Suppress("UNCHECKED_CAST")
+                    return HomeViewModel(
+                        userRepository = UserRepository()
+                    ) as T
+                }
+            }
     }
 
 
